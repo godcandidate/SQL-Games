4b0c90582b341921fe6ad7fb6fe2bef7f4198ec0
godcandidate <godcandidate@gmail.com>
2023-07-22 14:12:19 +0300

    update 11-test.sql

--- sql-island/11-test.sql
+++ sql-island/11-test.sql
@@ -1,3 +1,4 @@
 -- set the ownership for ring and teaport items to owner with id 15
 UPDATE item SET owner = 15 WHERE item IN('ring', 'teapot')
+-- OR
 UPDATE item SET owner = 15 WHERE item = 'ring' OR item = 'teapot'
